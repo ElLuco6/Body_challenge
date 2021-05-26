a9d7701087c69f95ed23b688e3c0ef69320f56b2
CedricHourcade <cedric.hourcade1@gmail.com>
2021-05-26 16:27:15 +0200

    Images ajouter

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CoachController extends AbstractController
 {
@@ -60,6 +61,100 @@ class CoachController extends AbstractController
             'coaches'=> $coaches,
             'commentForm'=> $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/mon-compte/update", name="app_update")
+     
+     */
+    public function update(UserPasswordEncoderInterface $encoder) 
+    {
+        // Utilisateur non connecté
+        if(empty($this->getUser())){
+            return $this->redirectToRoute('app_home');
+        }
+        
+        $errors = [];
+       
+        $em = $this->getDoctrine()->getManager(); // Connexion
+        // $this->getUser() contient l'utilisateur actuellement connecté. Je peux donc le passé dans le find()
+        $user = $em->getRepository(User::class)->find($this->getUser());
+
+
+
+        if(!empty($_POST)){ // Mon formulaire n'est pas vide
+            $safe = array_map('trim', array_map('strip_tags', $_POST));
+
+            if(strlen($safe['lastName']) < 3 || strlen($safe['lastName']) > 120){
+                $errors['lastName'] = 'Le nom doit comporter entre 3 et 120 caractères';
+            }
+            if(strlen($safe['firstName']) < 3 || strlen($safe['firstName']) > 120){
+                $errors['firstName'] = 'Le prenom doit comporter entre 3 et 120 caractères';
+            }
+            if(strlen($safe['city']) < 3 || strlen($safe['city']) > 120 || !ctype_alpha($safe['city'])) {
+                $errors['city'] = 'Votre ville doit comporter entre 3 et 120 caractères et doit etre alphabetique';
+            }
+            if(empty($safe['description'])){
+                $errors['description'] = 'La description ne peut être vide';
+            }
+            if(empty($safe['experience'])){
+                $errors['experience'] = "L'experience ne peut être vide";
+            }
+            if(empty($safe['education'])){
+                $errors['education'] = 'La formation ne peut être vide';
+            }
+            if(empty($safe['sport'])){
+                $errors['sport'] = 'La spécialité ne peut être vide';
+            }
+            if(!is_numeric($safe['tarif']) || $safe['tarif'] <= 0){
+                $errors['tarif'] = 'Le tarif est invalide';
+            }
+            if(!is_numeric($safe['age']) || $safe['age'] <= 0){
+                $errors['age'] = "L'age est invalide";
+            }
+            if (!isset($safe['email']) || filter_var($safe['email'],FILTER_VALIDATE_EMAIL) == false) {
+                $errors['email'] = 'Votre email est invalide';
+            }
+
+            if(!empty($safe['password'])){
+                if (!isset($safe['password']) || !ctype_alnum($safe['password']) || strlen($safe['password']) < 8) {
+                    $errors['password'] = 'Le mot de passe doit etre aplhanumérique et supérieur à 8 caractere';
+                }
+            }
+
+             // Ici mon tableau $errors n'a aucune entrée 
+             if(count($errors) === 0){
+                // Alors je peux enregistrer en base de données
+
+                $user->setLastName($safe['lastName']);
+                $user->setFirstName($safe['firstName']);
+                $user->setEmail($safe['email']);
+                $user->setAge($safe['age']);
+                // Mise à jour du mot de passe seulement si rempli
+                if(!empty($safe['password'])){
+                    $hash = $encoder->encodePassword($user, $safe['password']);
+                    $user->setPassword($hash);
+                }
+                $user->setDescription($safe['description']);
+                $user->setCity($safe['city']);
+                $user->setSport($safe['sport']);
+                $user->setExperience($safe['experience']);
+                $user->setEducation($safe['education']);
+                $user->setTarif($safe['tarif']);
+                //$user->setCreatedAt(new \DateTime('now')); // La date & heure de l'instant T
+                
+                $em->persist($user);
+                $em->flush(); // Execute la requete (equivalent du $bdd->execute())
+            }
+            else {
+                // Afficher les erreurs
+            }
+        }
+        return $this->render('coach/update.html.twig', [
+            'update' => $user,
+            'errors' => $errors
+        ]);
+        
     }
 
 }
