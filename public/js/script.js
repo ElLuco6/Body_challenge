4c04f3f71775b565a150af43dc70c94dd6231782
ElLuco6 <saintsuperylucas@gmail.com>
2021-05-27 09:04:27 +0200

    js

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+
+// function hideDiv() {
+// 	document.getElementById("convocation").style.display="none";
+// }	
+// function showDiv() {
+// 	document.getElementById("convocation").style.display="bloc";
+// }	
+
+// $(document).ready(function(){
+// $('#documentDropdown').on('change', function(){
+//    if (this.value == '1')
+//    {
+//      $('#vka').show();
+//    }
+//     else {
+//      $('#vka').hide();
+//    }
+// });
+// });
+
+var p = document.querySelectorAll("h1");
+p.addEventListener("click", function() {
+    // Le code ci-dessous est exécuté quand on clique sur H1
+    p.style.color = "blue";
+});
+
