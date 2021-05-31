048ea2e8439b0c1fb53caddedfdc1852a475f80d
CedricHourcade <cedric.hourcade1@gmail.com>
2021-05-31 11:32:05 +0200

    commit

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CoachController extends AbstractController
